db43c48d935b3b6c559113535ee735bf13bbe4f2
Ryan Brubaker <ryan.brubaker@gmail.com>
2012-10-29 22:17:21 -0500

    Adding initial rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitTheGist::Application.config.secret_token = '9bc7e8975ea37bfbd478c20021e03d75cfb1c688e1741bd3e540d7f07d7fb9bae3acab28eebc3aa18ac7f8694cc6e9cfe51095bdfea745159cd2a114579046c4'
