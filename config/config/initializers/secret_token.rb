550bc4eee0b1867c2ebb03d1ec7d1f1b1acafd01
Ryan Brubaker <ryan.brubaker@gmail.com>
2012-10-29 21:53:20 -0500

    Skeleton rails app

---
+++ config/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitTheGist::Application.config.secret_token = 'a3cfc376083b664f8ee9e4b1b4dc4a53388e36c8f90c72aa4864689f66828065e51d661020569ccc87b1c9d969a83a893a955a76dfe9ff4a7a17bfcedb58370f'
